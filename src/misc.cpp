498d893bdba469a2e33eb7f8b4223ba97c880965
Oleg Volkov <olegv142@gmail.com>
2020-09-15 14:34:28 +0300

    Yet another useful matrix function

--- src/misc.cpp
+++ src/misc.cpp
@@ -1,5 +1,11 @@
 #include "misc.h"
+#include "nrutil.h"
+#include "nrtools.h"
+#include "math.h"
 
+DefineErrorMess( EMOD_IT, "Too many iterations in emod()" );
+
+/* Count zero crossings given the vector f[1..r] */
 unsigned count_zeros(float const *f, int l, int r, float delta)
 {
 	unsigned cnt = 0;
@@ -16,3 +22,60 @@ unsigned count_zeros(float const *f, int l, int r, float delta)
 	}
 	return cnt;
 }
+
+/* Find matrix m[1..n][1..n] with the set of eigenvalues that may differ from eigenvalues of a[r..r+n-1][c..c+n-1]
+ * by the sign of the real part such that all m eigenvalues have positive real part.
+ */
+void emod(float const * const *a, int r, int c, int n, float conv, int itmax, float **m)
+{
+	float ** mi = matrix(1, n, 1, n);
+	float ** a2 = matrix(1, n, 1, n);
+	float sum;
+	int i, j, k, it;
+
+	clear_matrix(m, 1, n, 1, n);
+	for (i = 0; i < n; ++i)
+	{
+		sum = 0;
+		for (k = 0; k < n; ++k)
+			sum += fabs(a[r+i][c+k] * a[r+k][c+i]);
+		// Build initial guess
+		m[1+i][1+i] = sqrt(sum);
+		for (j = 0; j < n; ++j)
+		{
+			sum = 0;
+			for (k = 0; k < n; ++k)
+				sum += a[r+i][c+k] * a[r+k][c+j];
+			// Build a^2 matrix
+			a2[1+i][1+j] = sum;
+		}
+	}
+	/* Solve a^2 = m^2 equation using Babylonian iteration */
+	for (it = 0; it < itmax; ++it)
+	{
+		float x, err, max_err = 0;
+		copy_matrix(m, mi, 1, n, 1, n, 1, 1);
+		gaussj(mi, n, NULL, 0);
+		for (i = 1; i <= n; ++i)
+		{
+			for (j = 1; j <= n; ++j) {
+				sum = 0;
+				for (k = 1; k <= n; ++k)
+					sum += a2[i][k] * mi[k][j] + mi[i][k] * a2[k][j];
+				x = m[i][j] / 2 + sum / 4;
+				err = fabs(x - m[i][j]);
+				if (err > max_err)
+					max_err = err;
+				m[i][j] = x;
+			}
+		}
+		if (max_err < conv)
+			break;
+	}
+
+	free_matrix(mi, 1, n, 1, n);
+	free_matrix(a2, 1, n, 1, n);
+	if (it >= itmax)
+		Signal(EMOD_IT);
+}
+
--- src/misc.h
+++ src/misc.h
@@ -1,3 +1,13 @@
 #pragma once
 
+#include "errs.h"
+
+/* Count zero crossings given the vector f[1..r] */
 unsigned count_zeros(float const *f, int l, int r, float delta);
+
+/* Find matrix m[1..n][1..n] with the set of eigenvalues that may differ from eigenvalues of a[r..r+n-1][c..c+n-1]
+ * by the sign of the real part such that all m eigenvalues have positive real part.
+ */
+void emod(float const * const *a, int r, int c, int n, float conv, int itmax, float **m);
+
+ErrorCode( EMOD_IT );
